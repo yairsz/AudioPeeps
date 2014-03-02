3cd3977daa03bbeb977e7750a4d3ad97974ffacd
Yair Szarf <yair@mac.com>
2014-03-01 16:51:32 -0800

    added audio exporter class

---
+++ AudioPeeps/YSAudioExporter.h
@@ -0,0 +1,13 @@
+//
+//  YSAudioExporter.h
+//  AudioPeeps
+//
+//  Created by Yair Szarf on 3/1/14.
+//  Copyright (c) 2014 The 2 Handed Consortium. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YSAudioExporter : NSObject
+
+@end
---
+++ AudioPeeps/YSAudioExporter.m
@@ -0,0 +1,30 @@
+//
+//  YSAudioExporter.m
+//  AudioPeeps
+//
+//  Created by Yair Szarf on 3/1/14.
+//  Copyright (c) 2014 The 2 Handed Consortium. All rights reserved.
+//
+
+#import "YSAudioExporter.h"
+
+@interface YSAudioExporter ()
+//@property (strong,nonatomic) NSStri<#string#>ng * ;
+
+@end
+
+@implementation YSAudioExporter
+
+
+
+//
+//NSString *serializationQueueDescription = [NSString stringWithFormat:@"%@ serialization queue", self];
+////// Create the main serialization queue.
+////self.mainSerializationQueue = dispatch_queue_create([serializationQueueDescription UTF8String], NULL);
+////NSString *rwAudioSerializationQueueDescription = [NSString stringWithFormat:@"%@ rw audio serialization queue", self];
+////// Create the serialization queue to use for reading and writing the audio data.
+////self.rwAudioSerializationQueue = dispatch_queue_create([rwAudioSerializationQueueDescription UTF8String], NULL);
+////NSString *rwVideoSerializationQueueDescription = [NSString stringWithFormat:@"%@ rw video serialization queue", self];
+////// Create the serialization queue to use for reading and writing the video data.
+////self.rwVideoSerializationQueue = dispatch_queue_create([rwVideoSerializationQueueDescription UTF8String], NULL);
+@end
